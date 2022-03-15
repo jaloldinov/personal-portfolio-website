9ddfbed72b12219659c29cef4bdce7d2dba8503e
Senior-Uz <omadbekclever2@gmail.com>
2022-03-16 00:53:05 +0500

    added service section

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,8 +68,9 @@ window.addEventListener("scroll", scrollActive);
 
 //  CHANGE BACKGROUND HEADER
 function scrollHeader() {
-  const header = document.getElementById("header");
-  if (this.scrollY >= 200) {
+  const home = document.getElementById("home");
+
+  if (this.scrollY >= home.offsetHeight) {
     header.classList.add("scroll-header");
   } else {
     header.classList.remove("scroll-header");
